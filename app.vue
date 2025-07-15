ba2b0dc7b70102c3dc04c197770fb38fad9da75e
ogaguinaga <115434003+ogaguinaga@users.noreply.github.com>
2025-07-14 18:40:07 -0600

    chore: add tailwind, daisy-ui and icons

--- app.vue
+++ app.vue
@@ -1,12 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold text-primary">
-      Hello
-    </h1>
-
-    <button class="btn btn-accent">
-      Some action
-      <Icon name="hugeicons:rocket-01" :size="24" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
